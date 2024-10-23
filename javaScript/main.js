c34e019924d247db46e45e5d4ecf74bd03d9be6f
Bruno-dr <bdrossi00@gmail.com>
2024-10-23 20:40:49 -0300

    primera version libreria

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,12 +1,13 @@
 let usuarios = ["Ruben",  "Marcos", "Carlos"];
 
 function inicio() {
-    let usuario = prompt("Ingrese su nombre de usuario:");
+    let usuario = prompt(" usuario:");
 
     if (usuarios.includes(usuario)) {
-        alert(`Bienvenido, ${usuario}!`);
+        alert(`Bienvenido, ${usuario}`);
     } else {
         let crearUsuario = prompt("Ingrese nuevo usuario");
+        
         if (crearUsuario) {
             usuarios.push(crearUsuario);
             if (usuarios.includes(crearUsuario)) {
@@ -22,24 +23,4 @@ inicio();
 
 
 
-// function ingreso() {
-//     let nombre = prompt("ingrese usuario:");
-//     let contraseña = prompt("ingrese usuario:");
-
-//     let edad = prompt("ingrese su edad:");
-
-//     if (edad >= 18) {
-//         alert(`Bienvenido, ${nombre}!`);
-//         carritoCompras();
-//     } else {
-//         alert("No puedes hacear compras.");
-//     }
-// }
-
-// function carritoCompras(){
-//     alert("la idea es que se pueda usar la funcion del carrito")
-//     console.log( "esta re verde todo , el proximo va  aestar mas completo")
-    
-// }
-
-// ingreso();
+//NOTA: ESta re verde todo, la proxima entrega va a a estar mas completa y entendible de que va el proyecto, pero creo q cumple con lo minimo, gracias.
